0797e6725f49d5a5508f24b61412636482720d10
sepehr mokhtar <113509443+itssepsi@users.noreply.github.com>
2024-04-14 19:48:39 +0200

    14 april

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,16 @@
-pub fn square_of_sum(n: u32) -> u32 {
-    unimplemented!("square of sum of 1...{n}")
-}
+fn main() {
+    let n = 10;
 
-pub fn sum_of_squares(n: u32) -> u32 {
-    unimplemented!("sum of squares of 1...{n}")
-}
+    let mut sum_of_squares = 0;
+    let mut sum = 0;
 
-pub fn difference(n: u32) -> u32 {
-    unimplemented!("difference between square of sum of 1...{n} and sum of squares of 1...{n}")
+    for i in 1..=n {
+        sum_of_squares += i * i;
+        sum += i;
+    }
+
+    let square_of_sum = sum * sum;
+    let difference = square_of_sum - sum_of_squares;
+
+    println!("Difference for the first {} natural numbers: {}", n, difference);
 }
